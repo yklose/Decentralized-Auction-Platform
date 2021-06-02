62e656981eeee87d5d2625bd64999a56fd3b5589
Yannick Klose <yannick.klose@icloud.com>
2021-06-02 14:30:57 +0200

    update for sealed auctions

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -1,30 +1,29 @@
-//pragma solidity ^0.5.8;
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract AuctionHouse {
 
     struct Auction {
         address owner;
+        uint user_identifier;
         uint endtime; 
         bool active; 
+        bool sealed_auction; 
         address[] bidders;
-        mapping (address => uint) bids;
         mapping (address => uint) deposit;   
+        mapping (address => uint) bids;
     }
 
     Auction[] auctions;
 
     // define variables
     uint256 ether_var = 10**18;
-    uint256 max_num_contracts = 10**10;
     uint256 interval = 7;
+    
 
-    // Log the event about a deposit being made by an address and its amount
-    event LogDepositMade(address indexed accountAddress, uint amount);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    constructor() public payable{
-        // TODO: add something here
+    function hashSeriesNumber(uint256 nonce, uint256 number) public pure returns (bytes32) {
+        // hash the value of a string and number 
+        return keccak256(abi.encode(number, nonce));
     }
 
     function get_owner(uint idx) public view returns (address){
@@ -33,6 +32,7 @@ contract AuctionHouse {
     }
 
     function is_owner(address add, uint idx) public view returns (bool){
+        // returns true/false of an address with idx
         if (add == auctions[idx].owner){
             return true;
         }
@@ -41,30 +41,59 @@ contract AuctionHouse {
         }
     }
 
-    function get_id(address owner) public view returns (uint){
+    function is_sealed(uint idx) public view returns (bool){
+        // returns true/false if the auction is sealed
+        if (auctions[idx].sealed_auction == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function get_idx_from_identifier(uint identifier) public view returns (uint){
+        // get the id of an auction given an identifier
+        require(identifier_is_unused(identifier)==false);
         for (uint256 idx = 0; idx < auctions.length; idx++) {
-            if (is_owner(owner, idx)){
+            if (auctions[idx].user_identifier == identifier){
                 return idx;
             }
         }
-        return max_num_contracts+1; // TODO: larger value
+        return 0;
     }
 
-    function deploy_auction() public payable{
-        // deploy an auction
-        uint idx = auctions.length;
-        // check if maximum number of contracts is reached
-        if (idx < max_num_contracts){
-            auctions.push();
-            // define parameters
-            Auction storage new_auct = auctions[idx];
-            new_auct.endtime = block.number + interval;
-            new_auct.owner = msg.sender;
+    function identifier_is_unused(uint identifier) public view returns (bool){
+        // check if the identifier is already in use
+        for (uint256 idx = 0; idx < auctions.length; idx++) {
+            if (auctions[idx].user_identifier == identifier){
+                return false;
+            }
         }
+        return true;
+    }
+
+    function get_number_of_auctions() public view returns (uint){
+        // returns the number of auctions
+        return auctions.length;
+    }
+
+    function deploy_auction(uint identifier, bool type_sealed) public payable returns (uint){
+        // check if identifier is already used
+        require(identifier_is_unused(identifier));
+        // deploy auction
+        uint idx = auctions.length;
+        auctions.push();
+        // define parameters
+        Auction storage new_auct = auctions[idx];
+        new_auct.endtime = block.number + interval;
+        new_auct.owner = msg.sender;
+        new_auct.sealed_auction = type_sealed;
+        new_auct.user_identifier = identifier;
+        return idx;
     }
 
     function start_auction(uint idx) public payable {
-        //start the auction, setting timelimit, can only not be reactivated
+        // start the auction, setting timelimit, can only not be reactivated
         if (msg.sender == auctions[idx].owner && auctions[idx].active == false){
             auctions[idx].endtime = block.number + interval;
             auctions[idx].active = true;
@@ -83,7 +112,7 @@ contract AuctionHouse {
         return auctions[idx].deposit[msg.sender];
     }
 
-    function set_bid(uint idx, uint bid_value) public {
+    function set_bid(uint idx, uint bid_value, uint nonce) public {
         // contract has to be active
         if (auctions[idx].active){
             // the owner can not bid
@@ -94,7 +123,12 @@ contract AuctionHouse {
                     if (bid_value > auctions[idx].bids[msg.sender]) {
                         // only accept bid if in valid time period
                         if (auctions[idx].endtime > create_timestamp()){
-                            auctions[idx].bids[msg.sender] = bid_value;
+                            if (auctions[idx].sealed_auction == true){
+                                auctions[idx].bids[msg.sender] = uint256(hashSeriesNumber(nonce, bid_value));
+                            }
+                            else{
+                                auctions[idx].bids[msg.sender] = bid_value;
+                            }
                         }
                     }
                 }
@@ -130,7 +164,9 @@ contract AuctionHouse {
     }    
 
     function get_winner(uint idx) public view returns (address, uint) {
+        // gets the winner in an open price auction
         require((auctions[idx].endtime < block.number));
+        require(auctions[idx].sealed_auction == false);
         uint winning_bid=0;
         address winner_address=get_owner(idx);
         for (uint256 i = 0; i < auctions[idx].bidders.length; i++) {
@@ -144,10 +180,9 @@ contract AuctionHouse {
     }
 
     function refund_deposit(uint idx) public {
-        // refund the deposit if action is not active 
-        require(auctions[idx].active == false);
+        // refund the deposit if action is not active or if time is over
+        require(auctions[idx].active == false || auctions[idx].endtime < block.number);
         payable(msg.sender).transfer(auctions[idx].deposit[msg.sender]);
         auctions[idx].deposit[msg.sender] = 0;
     }
-
 }
--- test/auctionhouse.test.js
+++ test/auctionhouse.test.js
@@ -8,31 +8,34 @@ contract("AuctionHouse - basic initialization", function(accounts) {
   const bob = accounts[1];        // Bidder
   const charlie = accounts[2];    // Bidder
   const dave = accounts[3];       // Bidder
+
+  let auction;
+  before(async () => {
+    auction = await AuctionHouse.deployed();
+  });
+
   
   describe("Testing Auction", async () => {
-    it("Simple initialization of contract", async () => {
-      const auction = await AuctionHouse.deployed();
-    });
-
+    
     it("Deploy two contracts with auction IDs", async () => {
-      const auction = await AuctionHouse.deployed();
-      await auction.deploy_auction({from: alice});
-      await auction.deploy_auction({from: bob});
+      // Use special user identifier! 
+      await auction.deploy_auction(0000, false, {from: alice});
+      await auction.deploy_auction(0001, false, {from: bob});
+
     });
 
     it("Deploy two contracts and let bidders deposit money and refund before auction started", async () => {
-      const auction = await AuctionHouse.deployed();
-      await auction.deploy_auction({from: alice});
-      await auction.deploy_auction({from: bob});
+      //const auction = await AuctionHouse.deployed();
+      await auction.deploy_auction(0002, false, {from: alice});
+      await auction.deploy_auction(0003, false, {from: bob});
 
-      // these functions can be called by anyone!
       var auction_id0;
-      await auction.get_id.call(alice, {from: alice}).then(function(idx){
+      await auction.get_idx_from_identifier.call(0002, {from: alice}).then(function(idx){
         auction_id0 = idx;
       });
       var auction_id1;
-      await auction.get_id.call(bob, {from: alice}).then(function(idx){
-        auction_id1 = idx
+      await auction.get_idx_from_identifier.call(0003, {from: alice}).then(function(idx){
+        auction_id1 = idx;
       });
 
       // let bidders deposit money on auction 0
@@ -52,21 +55,41 @@ contract("AuctionHouse - basic initialization", function(accounts) {
       await auction.refund_deposit(auction_id1, {from: charlie});
       await auction.refund_deposit(auction_id1, {from: dave});
 
+      // check if deposit is zero for auction
+      await auction.get_deposit_balance.call(auction_id0, {from: bob}).then(function(balance){
+        assert.equal(balance.toString(), "0");
+      });
+      await auction.get_deposit_balance.call(auction_id0, {from: charlie}).then(function(balance){
+        assert.equal(balance.toString(), "0");
+      });
+      await auction.get_deposit_balance.call(auction_id0, {from: dave}).then(function(balance){
+        assert.equal(balance.toString(), "0");
+      });
+      await auction.get_deposit_balance.call(auction_id1, {from: alice}).then(function(balance){
+        assert.equal(balance.toString(), "0");
+      });
+      await auction.get_deposit_balance.call(auction_id1, {from: charlie}).then(function(balance){
+        assert.equal(balance.toString(), "0");
+      });
+      await auction.get_deposit_balance.call(auction_id1, {from: dave}).then(function(balance){
+        assert.equal(balance.toString(), "0");
+      });
+
     });
-
+    
     it("Full test with two auctions 3 bidders and winner determination", async () => {
-      const auction = await AuctionHouse.deployed();
-      await auction.deploy_auction({from: alice});
-      await auction.deploy_auction({from: bob});
+      // Use special user identifier for deploying an auction
+      await auction.deploy_auction(1111, false, {from: alice});
+      await auction.deploy_auction(2222, false, {from: bob});
 
-      // these functions can be called by anyone!
+      // get auction id on blockchain using the identifier
       var auction_id0;
-      await auction.get_id.call(alice, {from: alice}).then(function(idx){
+      await auction.get_idx_from_identifier.call(1111, {from: alice}).then(function(idx){
         auction_id0 = idx;
       });
       var auction_id1;
-      await auction.get_id.call(bob, {from: alice}).then(function(idx){
-        auction_id1 = idx
+      await auction.get_idx_from_identifier.call(2222, {from: alice}).then(function(idx){
+        auction_id1 = idx;
       });
 
       // let bidders deposit money on auction 0
@@ -82,33 +105,31 @@ contract("AuctionHouse - basic initialization", function(accounts) {
       await auction.start_auction(auction_id1, {from: bob})
 
       // -------------------------- BIDDING EXAMPLE --------------------------
-      
       // Alice Bids: 
       const bidAlice1 = 15;
-      await auction.set_bid(auction_id1, bidAlice1, {from: alice});
+      await auction.set_bid(auction_id1, bidAlice1, 0, {from: alice});
       // Bob Bids:
       const bidBob0 = 40;
-      await auction.set_bid(auction_id0, bidBob0, {from: bob});
+      await auction.set_bid(auction_id0, bidBob0, 0, {from: bob});
       // wait for 1 second
       await new Promise(r => setTimeout(r, 3000));
       // Charlie Bids:
       const bidCharlie0 = 38;
-      await auction.set_bid(auction_id0, bidCharlie0, {from: charlie});
+      await auction.set_bid(auction_id0, bidCharlie0, 0, {from: charlie});
       const bidCharlie1 = 14;
-      await auction.set_bid(auction_id1, bidCharlie1, {from: charlie});
+      await auction.set_bid(auction_id1, bidCharlie1, 0, {from: charlie});
       // Dave Bids:
       const bidDave0 = 18;
-      await auction.set_bid(auction_id0, bidDave0, {from: dave});
+      await auction.set_bid(auction_id0, bidDave0, 0, {from: dave});
       const bidDave1 = 19;
-      await auction.set_bid(auction_id1, bidDave1, {from: dave});
+      await auction.set_bid(auction_id1, bidDave1, 0, {from: dave});
       // wait for another 2 seconds (should be outside the interval)
       await new Promise(r => setTimeout(r, 2000));
       // Dave updates bids 44 ether (should be outside the time interval)
       const bidDaveUpdate = 44;
-      await auction.set_bid(auction_id0, bidDaveUpdate, {from: dave});
+      await auction.set_bid(auction_id0, bidDaveUpdate, 0, {from: dave});
 
-      // -------------------------- STATISTICS --------------------------
-
+      // ----------------------------- STATISTICS -----------------------------
       // get highest bids of all bidders (auction id=0)
       const EndBidBob       = await auction.get_bid(auction_id0,{from: bob});
       const EndBidCharlie   = await auction.get_bid(auction_id0,{from: charlie});
@@ -117,37 +138,75 @@ contract("AuctionHouse - basic initialization", function(accounts) {
       const EndBidAlice1     = await auction.get_bid(auction_id1,{from: alice});
       const EndBidCharlie1   = await auction.get_bid(auction_id1,{from: charlie});
       const EndBidDave1      = await auction.get_bid(auction_id1,{from: dave});
- 
       // show results for action 0
       console.log("");
       console.log("--------- Highest bids for auction 0 ---------");
-      console.log("");
       console.log("Bobs      Bid: ", EndBidBob.toString());
       console.log("Charlies  Bid: ", EndBidCharlie.toString());
       console.log("Daves     Bid: ", EndBidDave.toString());
-      console.log("");
       // Get winner
       await auction.get_winner.call(auction_id0, {from: alice}).then(function(result){
         console.log("Auction_ID=%d, Address=%s, Winning_bid=%d", auction_id0, result[0], result[1]);
       });
       // The owner of the action id can kill the auction (deposits are payed back automatically)
       await auction.kill(auction_id0, {from: alice});
-
       // show results for action 1
       console.log("");
       console.log("--------- Highest bids for auction 1 ---------");
-      console.log("");
       console.log("Alices    Bid: ", EndBidAlice1.toString());
       console.log("Charlies  Bid: ", EndBidCharlie1.toString());
       console.log("Daves     Bid: ", EndBidDave1.toString());
-      console.log("");
       // Get winner
       await auction.get_winner.call(auction_id1, {from: alice}).then(function(result){
         console.log("Auction_ID=%d, Address=%s, Winning_bid=%d", auction_id0, result[0], result[1]);
       });
+      // Bob requests back his refund
+      await auction.refund_deposit(auction_id0, {from: bob});
       // The owner of the action id can kill the auction (deposits are payed back automatically)
       await auction.kill(auction_id1, {from: bob});
 
+    });
+
+    it("Deploy one sealed auction with published hash value", async () => {
+      // Use special user identifier for deploying an auction
+      await auction.deploy_auction(3333, true, {from: alice});
+      // these functions can be called by anyone!
+      var auction_id0;
+      await auction.get_idx_from_identifier.call(3333, {from: alice}).then(function(idx){
+        auction_id0 = idx;
+      });
+    });
+
+    it("Deploy one sealed auction with published hash value", async () => {
+      // Use special user identifier for deploying an auction
+      await auction.deploy_auction(4444, true, {from: alice});
+      // these functions can be called by anyone!
+      var auction_id0;
+      await auction.get_idx_from_identifier.call(4444, {from: alice}).then(function(idx){
+        auction_id0 = idx;
+      });
+      // let bidders deposit money on auction 0
+      await auction.deposit_money(auction_id0, {from: bob, value: web3.utils.toBN(5*ether)});
+      await auction.deposit_money(auction_id0, {from: charlie, value: web3.utils.toBN(5*ether)});
+      await auction.deposit_money(auction_id0, {from: dave, value: web3.utils.toBN(5*ether)});
+      // start auction
+      await auction.start_auction(auction_id0, {from: alice})
+      // -------------------------- BIDDING EXAMPLE --------------------------
+      const bidCharlie = 14;
+      await auction.set_bid(auction_id0, bidCharlie, 0, {from: charlie});
+      const bidBob = 40;
+      await auction.set_bid(auction_id0, bidBob, 0, {from: bob});
+      const bidDave = 40;
+      await auction.set_bid(auction_id0, bidDave, 1, {from: dave});
+
+      const hashBidCharlie = await auction.get_bid(auction_id0, {from: charlie});
+      const hashBidBob = await auction.get_bid(auction_id0, {from: bob});
+      const hashBidDave = await auction.get_bid(auction_id0, {from: dave});
+
+      console.log("Charlies Hash Bid : ", hashBidCharlie.toString());
+      console.log("Bobs Hash Bid     : ", hashBidBob.toString());
+      console.log("Daves Hash Bid    : ", hashBidDave.toString());
+      // ---------------------------------------------------------------------
     });
     
   });
