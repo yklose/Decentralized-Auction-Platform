8221115f8af1dc86e82b49debc17e316e3d3b7c6
umbrellerde <trever@umbreller.de>
2021-06-03 10:47:58 +0200

    Add API

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,24 @@
 var express = require('express');
-var queryable = require('queryable');
 var router = express.Router();
+
+var queryable = require('queryable');
 var db = queryable.open('auctionhouse.db');
+
+const ganache = require('ganache-core');
+const Web3 = require('web3');
+let web3 = new Web3(ganache.provider());
+
+import AuctionHouse from "../../build/src/contracts/AuctionHouse.json"
+const auctionHouseAddress = process.env.HOUSE_ADDR
+const auctionHouseAbi = AuctionHouse.abi
+  
+const networkId = await web3.eth.net.getId();
+const deployedNetwork = AuctionHouse.networks[networkId];
+const auctionHouse = new web3.eth.Contract(
+  AuctionHouse.abi,
+  deployedNetwork && deployedNetwork.address,
+);
+
 
 let auctionKeys = ['item', 'description']
 /**
@@ -21,20 +38,21 @@ function requireJsonKeys(jsonObject, keys) {
 
 /**
  * The auction house can create a new auction here
- * param id: the id that the auction has on-chain
  * body:
  *  'item' item on sale (string)
  *  'description' longer description (string, TODO Max maybe even HTML?)
+ * returns:
+ *  'id' Id of Contract on Chain
  */
-router.post('/auction/:id', function(req, res, next) {
-  let alreadyExists = db.find({'id': req.params.id});
-  if(alreadyExists.length != 0) {
-    res.status(400).send({
-      'msg': "This auction already exists, cannot create it",
-      'existing': alreadyExists
-    })
-    return
-  }
+router.post('/auction/', function(req, res, next) {
+  // let alreadyExists = db.find({'id': req.params.id});
+  // if(alreadyExists.length != 0) {
+  //   res.status(400).send({
+  //     'msg': "This auction already exists, cannot create it",
+  //     'existing': alreadyExists
+  //   })
+  //   return
+  // }
 
   if (!requireJsonKeys(req.body, auctionKeys)) {
     res.status(400).send({
@@ -45,16 +63,20 @@ router.post('/auction/:id', function(req, res, next) {
     return
   }
   
+  // TODO Create New Auction Here, Get ID Back
+  let auctionId = 0
   let newAuction = req.body
-  req.body.id = req.params.id
   db.insert(newAuction);
   db.save();
-  res.status(200).send(db.find({'id': req.params.id}).rows[0]);
+  res.status(200).send(db.find({'id': auctionId}).rows[0]);
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(db.find().rows);
+  res.send({
+    'db': db.find().rows,
+    'block': provider.getBlockNumber()
+  })
 });
 
 
