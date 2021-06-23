a18d4c900c900893e871c03a3db6e27c61f0450c
umbrellerde <trever@umbreller.de>
2021-06-23 17:08:10 +0200

    Refine Docker Integration

--- .dockerscripts/startup.sh
+++ .dockerscripts/startup.sh
@@ -2,6 +2,7 @@
 
 function prepend() { while read line; do echo "${1}${line}"; done; }
 
+(ganache-cli -h 0.0.0.0 |prepend "[ganache] ") &
+sleep 8 && # Start the backend after ganache has started
 (npm --prefix api/ run develop | prepend "[backend] ") &
-(npm --prefix build/ run start | prepend "[frontend] ") &
-(ganache-cli -h 0.0.0.0 |prepend "[ganache] ")
+(npm --prefix build/ run start | prepend "[frontend] ")
