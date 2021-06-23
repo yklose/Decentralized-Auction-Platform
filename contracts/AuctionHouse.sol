45ecf1cc67ecfe05564f60310488b49dfdf8507b
Yannick Klose <yannick.klose@icloud.com>
2021-06-23 19:56:25 +0200

    add require error messages

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -57,7 +57,7 @@ contract AuctionHouse {
 
     function get_idx_from_identifier(uint identifier) public view returns (uint){
         // get the id of an auction given an identifier
-        require(identifier_is_unused(identifier)==false);
+        require(identifier_is_unused(identifier)==false, "ERROR: Identifier is already used");
         for (uint256 idx = 0; idx < auctions.length; idx++) {
             if (auctions[idx].auction_identifier == identifier){
                 return idx;
@@ -83,7 +83,7 @@ contract AuctionHouse {
 
     function deploy_auction(uint identifier, bool type_sealed) public payable{
         // deploys an auction
-        require(identifier_is_unused(identifier));
+        require(identifier_is_unused(identifier), "ERROR: Identifier is already used");
         uint idx = auctions.length;
         auctions.push();
         Auction storage new_auct = auctions[idx];
@@ -96,8 +96,8 @@ contract AuctionHouse {
 
     function start_auction(uint idx) public payable {
         // start the auction, setting timelimit, can only not be reactivated
-        require(msg.sender == auctions[idx].owner);
-        require(auctions[idx].active == false);
+        require(msg.sender == auctions[idx].owner, "ERROR: Only the owner can start the auction");
+        require(auctions[idx].active == false, "ERROR: The auction can only be started once");
         auctions[idx].endtime = block.number + interval;
         auctions[idx].active = true;
         
@@ -118,17 +118,17 @@ contract AuctionHouse {
 
     function set_bid(uint idx, uint bid_value) public {
         // sets the bid, requirements have to be fulfilled 
-        require(auctions[idx].active);
-        require(msg.sender != auctions[idx].owner);
-        require(auctions[idx].deposit[msg.sender]>=5 ether);
-        require(auctions[idx].endtime > create_timestamp());
+        require(auctions[idx].active, "ERROR: The auction has to be started (active)");
+        require(msg.sender != auctions[idx].owner, "ERROR: The owner can not bid on his/her auction");
+        require(auctions[idx].deposit[msg.sender]>=5 ether, "ERROR: Deposit is not enough (at least 5 ETH)");
+        require(auctions[idx].endtime > create_timestamp(), "ERROR: Auction is already over (Block limit reached)");
 
         if (auctions[idx].sealed_auction == true){
             auctions[idx].bids[msg.sender] = bid_value;
             emit SealedBidEvent(msg.sender, idx);
         }
         else{
-            require(bid_value > auctions[idx].bids[msg.sender]);
+            require(bid_value > auctions[idx].bids[msg.sender], "ERROR: Bids have to be higher than previous bids (open auction)");
             auctions[idx].bids[msg.sender] = bid_value;
             emit OpenBidEvent(msg.sender, bid_value, idx);
         }
@@ -140,7 +140,7 @@ contract AuctionHouse {
     }
 
     function get_highest_bid(uint idx) public view returns (uint) {
-        require(auctions[idx].sealed_auction == false);
+        require(auctions[idx].sealed_auction == false, "ERROR: get highest bid can only be called on open auction");
         uint winning_bid=0;
         for (uint256 i = 0; i < auctions[idx].bidders.length; i++) {
             address curr_bidder = auctions[idx].bidders[i];
@@ -163,8 +163,8 @@ contract AuctionHouse {
 
     function kill(uint idx) public{
         // transfers back all deposts for auction id
-        require(msg.sender == auctions[idx].owner);
-        require(block.number > auctions[idx].endtime);
+        require(msg.sender == auctions[idx].owner, "ERROR: Only the owner can kill the auction.");
+        require(block.number > auctions[idx].endtime, "ERROR: The auction can only be killed when auction is over");
         for (uint256 i = 0; i < auctions[idx].bidders.length; i++) {
             address bidder = auctions[idx].bidders[i];
             uint deposit = auctions[idx].deposit[auctions[idx].bidders[i]];
@@ -175,8 +175,8 @@ contract AuctionHouse {
 
     function get_winner(uint idx) public view returns (address, uint) {
         // gets the winner in an open price auction
-        require((auctions[idx].endtime <= block.number));
-        require(auctions[idx].sealed_auction == false);
+        require(auctions[idx].endtime <= block.number, "ERROR: Get winner can only be called when auction is over");
+        require(auctions[idx].sealed_auction == false, "ERROR: The function get winner can only be called on open auctions.");
         uint winning_bid=0;
         address winner_address=get_owner(idx);
         for (uint256 i = 0; i < auctions[idx].bidders.length; i++) {
@@ -191,7 +191,7 @@ contract AuctionHouse {
 
     function refund_deposit(uint idx) public {
         // refund the deposit if action is not active or if time is over
-        require(auctions[idx].active == false || auctions[idx].endtime < block.number);
+        require(auctions[idx].active == false || auctions[idx].endtime < block.number, "ERROR: Refund is only possible after the auction is over or before the auction has started.");
         payable(msg.sender).transfer(auctions[idx].deposit[msg.sender]);
         auctions[idx].deposit[msg.sender] = 0;
     }
